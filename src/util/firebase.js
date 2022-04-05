304777579e9719111b62670acd65aa32beab9462
Alexander Nguyen <cernworks@gmail.com>
2022-04-05 08:42:27 -0400

    add database url

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -8,9 +8,10 @@ const firebaseConfig = {
     apiKey: "AIzaSyDSS9O67ZqBaKaqv9jO3hyU7RQeGZiRMxM",
     authDomain: "viiject.firebaseapp.com",
     projectId: "viiject",
+    databaseURL: "https://viiject-default-rtdb.firebaseio.com/",
     storageBucket: "viiject.appspot.com",
     messagingSenderId: "190255363196",
-    appId: "1:190255363196:web:4494022ec9a9b3d0fa85f9"
+    appId: "1:190255363196:web:4494022ec9a9b3d0fa85f9",
   };
 
 const app = initializeApp(firebaseConfig);
