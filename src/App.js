f19e386dc1ba2c3918cf6daeeaf1ef9d46dc318e
Alexander Nguyen <cernworks@gmail.com>
2022-04-04 22:45:01 -0400

    adding authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {app, db} from './util/firebase';
 import ToDo from './components/ToDo';
 
+import { getAuth } from "firebase/auth";
+import 'firebase/compat/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+const auth = getAuth(app)
 
 function App() {
+
+  const [user] = useAuthState(auth)
   return (
     <body>
     <div className="App">
-      <ToDo />
+      {user ? <ToDo /> : <div> not home yet </div>}
     </div>
     </body>
   );
