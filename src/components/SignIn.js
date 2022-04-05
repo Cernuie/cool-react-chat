22b5842cf2d83b11183e4856681cb27848ca28f8
Alexander Nguyen <cernworks@gmail.com>
2022-04-05 05:00:51 -0400

    sign in and sign out

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,7 +1,35 @@
 import React from 'react';
-
+import { app } from '../util/firebase';
+import 'firebase/compat/auth'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { FirebaseError } from 'firebase/app';
 export default function SignIn(){
-    return(
+
+    const googleSignIn = () => {
+        const auth = getAuth(app);
+        const provider = new GoogleAuthProvider()
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // ...
+          }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+          });
+        }
+    return(<>
         <div> not signed in yet: </div>
+        <button classname="signIn" onClick={googleSignIn}>Sign In With Google</button>
+        </>
     )
 }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import firebase from '../util/firebase';
-
+import { app } from '../util/firebase';
+import { getAuth } from "firebase/auth"
 export default function toDo({ todo }){
+    const auth = getAuth(app);
 
+    const SignOut = () => {
+        return auth.currentUser && (
+            <button className='signOut' onClick={() => auth.signOut()}>Sign Out</button>
+        )
+    }
     return(
         <div>
             <h1>Here</h1>
+            <SignOut />
         </div>
     )
 
